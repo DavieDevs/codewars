4863fb79484dcc67949e6c6b1b3256431c91c7a9
DavieDevs <daviedevs@gmail.com>
2025-10-23 23:50:52 -0500

    added additional solution

--- 8kyu/ArrayPlusArray.js
+++ 8kyu/ArrayPlusArray.js
@@ -32,3 +32,9 @@ console.log(addTwo(arr1, arr2));
 function arrayPlusArray(arr1, arr2) {
   return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
 }
+
+// I was happy to find a solution that was similiar to mine:
+function arrayPlusArray(arr1, arr2) {
+  let arr = [...arr1, ...arr2];
+  return arr.reduce((a, b) => a + b);
+}
