a0acdae2784f128eda76344d8ff58f5f20b60839
DavieDevs <daviedevs@gmail.com>
2026-01-18 23:41:35 -0600

    Completed CountingSheep.js

---
+++ 8kyu/CountingSheep.js
@@ -0,0 +1,73 @@
+/*
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+*/
+
+/*
+-----------
+My Solution
+-----------
+
+Given ar array of true or false
+
+True is sheep that are present
+False is sheep that are missing
+
+Return the num of true values in an array.
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+*/
+
+let example = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
+
+const CountingSheep = (arr) => arr.filter(Boolean).length;
+
+console.log(CountingSheep(example));
+
+// Try count missing
+const CountingMissing = (arr) => arr.filter((item) => item === false).length;
+console.log(CountingMissing(example));
